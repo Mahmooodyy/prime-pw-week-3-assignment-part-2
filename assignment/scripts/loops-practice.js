0ddb5b321fd32debfe7c112ff70b968c435f999b
Mahmoud Mahmoud <mempltw2024@gmail.com>
2022-05-18 20:21:05 -0500

    figured out how to do the while statements made the right variables and constants before moving forward with the actual statement

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -46,16 +46,26 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (const star of stars) {
+  console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+while (stars) {
+  console.log(stars);
+  stars++
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let index = 0
+const max = 6
+while (index < max) {
+  console.log(index);
+  index++
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
