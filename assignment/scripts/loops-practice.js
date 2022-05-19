ba755f213c85b567e0124b1457417d262427e787
Mahmoud Mahmoud <mempltw2024@gmail.com>
2022-05-18 19:32:48 -0500

    made an array counting down from 5 to 0 and made a for loop setting i = to the array length - 1

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -28,14 +28,17 @@ for (let i=3; i<6; i++) {
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
-//   - set 
+//   - set i=2 and i<11 so it would log 10 and changed i+=2 so the value would go up by 2 every time
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for (let i=2; i<11; i+=2) {  
   console.log(i);
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+let array = [5,4,3,2,1,0]
+for (i = array.length - 1; i >= 0; i--) {
+    console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
